74a6e539076f0c7a19b37398e39ed235c963c4e9
Patricio Melo <42676797+patriciomelor@users.noreply.github.com>
2025-04-09 16:37:13 -0400

    Agregar campos 'apellido' y 'nro_telefono' al modelo de usuario

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -19,7 +19,9 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'apellido', 
         'email',
+        'nro_telefono',
         'password',
     ];
 
